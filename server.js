8606baa3cb9a6aaa36b85b7c3144c3697594b68a
omamonaphtali <omamonaphtali@gmail.com>
2018-09-04 20:44:23 +0300

    hashed out force SSL

--- server.js
+++ server.js
@@ -2,18 +2,18 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const forceSSL = function() {
-  return (req, res, next) => {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
+// const forceSSL = function() {
+//   return (req, res, next) => {
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//       return res.redirect(
+//        ['https://', req.get('Host'), req.url].join('')
+//       );
+//     }
+//     next();
+//   }
+// }
 
-app.use(forceSSL());
+// app.use(forceSSL());
 
 app.use(express.static('./dist/pwo'));
 
