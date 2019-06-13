ce24bf1f7b37ad1d7dcd1d896f972d98c5e72dca
omamonaphtali <omamonaphtali@gmail.com>
2019-06-13 23:13:40 +0300

    configured for heroku deploy

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var less = require('less-middleware');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const less = require('less-middleware');
 
-var indexRouter = require('./routes/index');
-var galleryRouter = require('./routes/gallery');
+const indexRouter = require('./routes/index');
+const galleryRouter = require('./routes/gallery');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(less(path.join(__dirname + '/public'), {compress: true})); //, [{compress: true}] 
+app.use(less(path.join(`${__dirname}/public`)));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/gallery', galleryRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -40,4 +41,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running at port: ${port}`);
+});
