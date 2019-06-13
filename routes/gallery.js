11ae85d6a488c556e1288d3d9c69e2789d01fecf
omamonaphtali <omamonaphtali@gmail.com>
2019-06-14 00:38:32 +0300

    formatting

--- routes/gallery.js
+++ routes/gallery.js
@@ -1,44 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('gallery/gallery', {title: 'Gallery | Peace Warriors Organization'})
-})
+  res.render('gallery/gallery', { title: 'Gallery | Peace Warriors Organization' });
+});
 
 router.get('/mentorship', (req, res) => {
-  res.render('gallery/mentorship', {title: 'Girls\' Mentorship | Peace Warriors Organization'})
-})
+  res.render('gallery/mentorship', { title: 'Girls\' Mentorship | Peace Warriors Organization' });
+});
 
 router.get('/kianyaga', (req, res) => {
-  res.render('gallery/kianyaga', {title: 'Kianyaga Children\'s Home Visit | Peace Warriors Organization'})
-})
+  res.render('gallery/kianyaga', { title: 'Kianyaga Children\'s Home Visit | Peace Warriors Organization' });
+});
 
 router.get('/peace-dinner', (req, res) => {
-  res.render('gallery/peace', {title: 'Peace Dinner | Peace Warriors Organization'})
-})
+  res.render('gallery/peace', { title: 'Peace Dinner | Peace Warriors Organization' });
+});
 
 router.get('/chebilat', (req, res) => {
-  res.render('gallery/chebilat', {title: 'Chebilat Town Peace Drive | Peace Warriors Organization'})
-})
-
-// function router () {
-//   const images = [
-//     {
-//       source: '/images/mentorship.png',
-//       tag: 'all',
-//       caption: 'Mentoring the future leaders',
-//     },
-//   ];
-
-//   galleryRouter.route('/')
-//     .get((req, res, next) => {
-//       res.render('gallery', 
-//         {
-//           title: "Gallery | Peace Warriors Organization",
-//           images: images,
-//         });
-//     });
-//   return galleryRouter;
-// }
+  res.render('gallery/chebilat', { title: 'Chebilat Town Peace Drive | Peace Warriors Organization' });
+});
 
 module.exports = router;
